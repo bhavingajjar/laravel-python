12ca5730d137f02776610053db9db41be80b3ced
Bhavin Gajjar <gajjarbhavin22@gmail.com>
2020-05-24 17:15:12 +0000

    Apply fixes from StyleCI

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,17 +2,16 @@
 
 namespace Bhavingajjar\LaravelPython\Tests;
 
-use Orchestra\Testbench\TestCase;
 use Bhavingajjar\LaravelPython\LaravelPythonServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class ExampleTest extends TestCase
 {
-
     protected function getPackageProviders($app)
     {
         return [LaravelPythonServiceProvider::class];
     }
-    
+
     /** @test */
     public function true_is_true()
     {
