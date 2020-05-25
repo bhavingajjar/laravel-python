a58e9acb5ba5eb05d2ff76594e39572d114b87b7
Bhavin Gajjar <gajjarbhavin22@gmail.com>
2020-05-25 14:45:51 +0530

    python resource path added
    python path added

--- config/config.php
+++ config/config.php
@@ -1,8 +1,31 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Python system install path
+    |--------------------------------------------------------------------------
+    |
+    | Set this value to the path of your system install python
+    | this is by default "null"
+    | Set this path like
+    | Example: 'C:\Python27\python.exe' or '/usr/bin/python' etc...
+    |
+    */
+    'python_path' => env('PYTHON_PATH', NULL),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Python Resource Path
+    |--------------------------------------------------------------------------
+    |
+    | Set this value to the path of your system install python
+    | this is by default "{project}/resources/python"
+    | Set this path like
+    | Example: '{project}/public/python' or '{project}/storage/python' etc...
+    |
+    */
+    'python_resource_path' => env('PYTHON_RESOURCE_PATH', resource_path('python')),
 
 ];
--- src/LaravelPython.php
+++ src/LaravelPython.php
@@ -7,27 +7,46 @@ use Symfony\Component\Process\Process;
 
 class LaravelPython
 {
+    public $pythonPath = null;
+    public $pythonResourcesPath = null;
+
     public function __construct()
     {
-
+        self::setPaths();
     }
 
-    public function run()
+    private function setPaths()
     {
-        $pythonPath = 'python';
-        if (PHP_OS === 'WINNT') {
-            exec('where python', $pythonPath);
-        } else {
-            exec('which python', $pythonPath);
+        $this->pythonPath = config('laravel-python.python_path');
+        if (empty($pythonPath)) {
+            if (PHP_OS === 'WINNT') {
+                exec('where python', $this->pythonPath);
+            } elseif (PHP_OS === 'Linux') {
+                exec('which python', $this->pythonPath);
+            } else {
+                $this->pythonPath = 'python';
+            }
         }
-        $process = Process::fromShellCommandline($pythonPath[0] . ' ' . public_path('script.py'));
-        $process->run();
+        if (is_array($this->pythonPath)) {
+            $this->pythonPath = $this->pythonPath[0];
+        }
 
+        $this->pythonResourcesPath = config('laravel-python.python_resource_path');
+    }
+
+    public function run($scriptName)
+    {
+        if (!file_exists($this->pythonResourcesPath . '/' . $scriptName)) {
+            return 'Python script not found';
+        }
+        $process = Process::fromShellCommandline($this->pythonPath . ' ' . $this->pythonResourcesPath . '/' . $scriptName);
+        $process->run();
+        $process->wait();
         // executes after the command finishes
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
-        
+
         return $process->getOutput();
     }
 }
